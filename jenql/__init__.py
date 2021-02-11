0e0d4889b520e7349313320e5a4f771c7c0d30ab
datadavev <datadavev@users.noreply.github.com>
2021-02-11 13:55:57 -0500

    POC for indexing service

---
+++ jenql/__init__.py
@@ -0,0 +1,109 @@
+import os
+import logging
+import tempfile
+import requests
+import glob
+import xml.etree.ElementTree as ET
+import rpyc
+import jnius_config
+
+_classpath_base = "apache-jena-3.17.0/lib/"
+_jars = ["."]
+for jar in glob.glob(f"{_classpath_base}*.jar"):
+    _jars.append(jar)
+try:
+    jnius_config.add_options("-Djava.awt.headless=true")
+    jnius_config.set_classpath(*_jars)
+except:
+    pass
+from jnius import autoclass
+
+QUERY_SOURCE = "https://raw.githubusercontent.com/DataONEorg/d1_cn_index_processor/develop_2.3/src/main/resources/application-context-schema-org.xml"
+
+
+class JenqlService(rpyc.Service):
+    def __init__(self, *args, **kwargs):
+        self.sparql_url = kwargs.pop("sparql_url")
+        super().__init__(*args, **kwargs)
+        self._sparql_queries = self._loadSparqlQueries(self.sparql_url)
+
+    def on_connect(self, conn):
+        pass
+
+    def on_disconnect(self, conn):
+        pass
+
+    def _loadSparqlQueries(self, query_source):
+        queries = {}
+        ns = {
+            "b": "http://www.springframework.org/schema/beans",
+        }
+        bean_config = ET.fromstring(requests.get(query_source).text)
+        beans = bean_config.findall("b:bean", ns)
+        for bean in beans:
+            cargs = bean.findall("./b:constructor-arg", ns)
+            qname = None
+            q = None
+            for carg in cargs:
+                if carg.attrib["name"] == "name":
+                    qname = carg.attrib["value"]
+                elif carg.attrib["name"] == "query":
+                    q = carg.find("./b:value", ns).text
+            if not qname is None:
+                queries[qname] = q.strip()
+        return queries
+
+    def _jentrify(self, jsonld):
+        L = logging.getLogger("jentrify")
+        L.debug("Starting jentrify...")
+        try:
+            dataManager = autoclass("org.apache.jena.riot.RDFDataMgr")
+            queryFactory = autoclass("org.apache.jena.query.QueryFactory")
+            queryExecutionFactory = autoclass(
+                "org.apache.jena.query.QueryExecutionFactory"
+            )
+        except Exception as e:
+            L.error(e)
+            return []
+        tmpf = tempfile.NamedTemporaryFile(delete=False, suffix=".jsonld")
+        fname = tmpf.name
+        tmpf.write(jsonld)
+        tmpf.close()
+        L.debug("jentrify fname= %s", fname)
+        dataset = dataManager.loadDataset(fname)
+        L.debug("dataset loaded")
+        idxresults = []
+        for term, query in self._sparql_queries.items():
+            L.debug("Running query %s", term)
+            row = {"query": query, "term": term, "v": []}
+            q = queryFactory.create(query)
+            qexec = queryExecutionFactory.create(q, dataset)
+            results = qexec.execSelect()
+            for res in results:
+                v = res.get(term)
+                if not v is None:
+                    row["v"].append(v.toString())
+                else:
+                    row["v"].append(res.toString())
+            idxresults.append(row)
+        os.unlink(fname)
+        return idxresults
+
+    def exposed_jentrify(self, jbytes):
+        if isinstance(jbytes, str):
+            jbytes = jbytes.encode("utf-8")
+        return self._jentrify(jbytes)
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    server = rpyc.utils.server.ThreadedServer(
+        JenqlService(sparql_url=QUERY_SOURCE),
+        port=9991,
+        protocol_config={"allow_public_attrs": True, "allow_pickle": True},
+    )
+    server.start()
+
+
+if __name__ == "__main__":
+    main()
--- sonormal/jldextract.py
+++ sonormal/jldextract.py
@@ -1,60 +1,16 @@
 """
 Extract JSON-LD
 """
-import os
 import logging
 import flask
-import copy
 import pyld
 import requests
 import json
-import tempfile
-import glob
+import rpyc
 from . import utils
 from . import normalize
-import xml.etree.ElementTree as ET
-
-'''
-import jnius_config
-_classpath_base = "apache-jena-3.17.0/lib/"
-_jars = ["."]
-for jar in glob.glob(f"{_classpath_base}*.jar"):
-    _jars.append(jar)
-try:
-    jnius_config.add_options("-Djava.awt.headless=true")
-    jnius_config.set_classpath(*_jars)
-except:
-    pass
-from jnius import autoclass
-'''
 
 jldex = flask.Blueprint("jldex", __name__, template_folder="templates/jldex")
-
-QUERY_SOURCE = "https://raw.githubusercontent.com/DataONEorg/d1_cn_index_processor/develop_2.3/src/main/resources/application-context-schema-org.xml"
-SPARQL_QUERIES = {}
-
-def loadSparqlQueries(query_source):
-    queries = {}
-    ns = {
-        "b": "http://www.springframework.org/schema/beans",
-    }
-    bean_config = ET.fromstring(requests.get(query_source).text)
-    beans = bean_config.findall("b:bean", ns)
-    for bean in beans:
-        cargs = bean.findall("./b:constructor-arg", ns)
-        qname = None
-        q = None
-        for carg in cargs:
-            if carg.attrib["name"] == "name":
-                qname = carg.attrib["value"]
-            elif carg.attrib["name"] == "query":
-                q = carg.find("./b:value", ns).text
-        if not qname is None:
-            queries[qname] = q.strip()
-    return queries
-
-#TODO: global var...
-SPARQL_QUERIES = loadSparqlQueries(QUERY_SOURCE)
 
 
 class objdict(dict):
@@ -90,14 +46,15 @@ def loadJsonLD(url):
     )
     return jsonld, resp
 
+
 def responseSummary(resp):
     rs = []
     for r in resp.history:
         row = {
             "url": r.url,
-            "status_code":r.status_code,
-            "content_type": r.headers.get('Content-Type', '-'),
-            "last-modified": r.headers.get('Last-Modified', '-'),
+            "status_code": r.status_code,
+            "content_type": r.headers.get("Content-Type", "-"),
+            "last-modified": r.headers.get("Last-Modified", "-"),
             "result": None,
         }
 
@@ -111,9 +68,9 @@ def responseSummary(resp):
     row = {
         "url": r.url,
         "status_code": r.status_code,
-        "content_type": r.headers.get('Content-Type', '-'),
-        "last-modified": r.headers.get('Last-Modified', '-'),
-        "result": None
+        "content_type": r.headers.get("Content-Type", "-"),
+        "last-modified": r.headers.get("Last-Modified", "-"),
+        "result": None,
     }
     loc = r.headers.get("Location", None)
     if loc is not None:
@@ -123,43 +80,13 @@ def responseSummary(resp):
     rs.append(row)
     return rs
 
-def jentrify(jsonld, queries):
-    L = flask.current_app.logger
-    L.debug("Starting jentrify...")
-    try:
-        dataManager = autoclass('org.apache.jena.riot.RDFDataMgr')
-        queryFactory = autoclass('org.apache.jena.query.QueryFactory')
-        queryExecutionFactory = autoclass('org.apache.jena.query.QueryExecutionFactory')
-    except Exception as e:
-        L.error(e)
-        return []
-    tmpf = tempfile.NamedTemporaryFile(delete=False, suffix=".jsonld")
-    fname = tmpf.name
-    tmpf.write(jsonld)
-    tmpf.close()
-    L.debug("jentrify fname= %s", fname)
-    dataset = dataManager.loadDataset(fname)
-    L.debug("dataset loaded")
-    idxresults = []
-    for term, query in queries.items():
-        L.debug("Running query %s", term)
-        row = {
-            "query": query,
-            "term": term,
-            "v": []
-        }
-        q = queryFactory.create(query)
-        qexec = queryExecutionFactory.create(q, dataset)
-        results = qexec.execSelect()
-        for res in results:
-            v = res.get(term)
-            if not v is None:
-                row["v"].append(v.toString())
-            else:
-                row["v"].append(res.toString())
-        idxresults.append(row)
-    os.unlink(fname)
-    return idxresults
+
+def jentrify(jbytes):
+    cli = rpyc.connect("localhost", 9991, config={"allow_all_attrs": True})
+    proxy = cli.root.jentrify(jbytes)
+    result = rpyc.utils.classic.obtain(proxy)
+    cli.close()
+    return result
 
 @jldex.route(
     "/",
@@ -182,13 +109,16 @@ def default():
         data.jresp = responseSummary(jresp)
 
         normalizer = normalize.SoNormalize()
-        data.jsonld_4, data.jsonld_3, data.jsonld_2, data.jsonld_1 = normalizer.normalizeSchemaOrg(data.jsonld)
+        (
+            data.jsonld_4,
+            data.jsonld_3,
+            data.jsonld_2,
+            data.jsonld_1,
+        ) = normalizer.normalizeSchemaOrg(data.jsonld)
         data.ids = normalize.extractIdentifiers(data.jsonld_4)
         data.hashes, jbytes = utils.jsonChecksums(data.jsonld_4)
         data.jbytes = jbytes.decode()
-
-        #data.indexed = jentrify(jbytes, SPARQL_QUERIES)
-        data.indexed = ["Not available."]
+        data.indexed = jentrify(jbytes)
 
     response = flask.make_response(flask.render_template("jldex.html", data=data))
     return response, 200
